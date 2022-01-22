253b78650686c406dc7c55415053428c3873fba8
fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-01-22 12:00:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,88 @@
+/*
+ * The Function Simply makes a request to the API and returns the response
+ * @param {string} url
+ * @return {promise}
+ */
+function fetchData(url) {
+	// API URL Usage: https://autocite.citation-api.com/api/v3/query?url={URL}
+	return fetch("https://autocite.citation-api.com/api/v3/query?url=" + url)
+		.then((response) => response.json())
+		.then(JSONTOMLA7)
+		.then(displayData);
+}
+
+/*
+ * This function simply takes the given data and extracts the needed information
+ * @param {object} data
+ * @return {void}
+ */
+
+function JSONTOMLA7(data) {
+	let csl = data.results[0].csl;
+	let author = csl.author.length ? csl.author[0].family : "",
+		title = csl.title,
+		titleShort = csl["title-short"],
+		publisher = csl.publisher,
+		issued = csl.issued["date-parts"] ? csl.issued["date-parts"] : "",
+		accessed = csl.accessed["date-parts"] ? csl.accessed["date-parts"] : "",
+		URL = csl.URL;
+	return { author, title, titleShort, publisher, issued, accessed, URL };
+}
+
+/*
+ * This functions simply displays the given data in the DOM
+ * @param {string} author
+ * @param {string} title
+ * @param {string} titleShort
+ * @param {string} publisher
+ * @param {string} issued
+ * @param {string} accessed
+ * @param {string} URL
+ * @return {void}
+ */
+function displayData({ author, title, titleShort, publisher, issued, accessed, URL }) {
+	let data = document.getElementById("Citation");
+	issued = dateToString(issued);
+	accessed = dateToString(accessed);
+	let string = "";
+	if (author) string += `${author}. `;
+	if (title) string += `"${title}" `;
+	if (titleShort) string += `${titleShort}.`;
+	else string += `. `;
+	if (publisher) string += ` ${publisher}, `;
+	if (issued) string += `${issued}. `;
+	if (accessed) string += `Web ${accessed}. `;
+	if (URL) string += `\n<${URL}>`;
+	data.innerText = string;
+}
+
+/*
+ * This function takes a date array and returns a string
+ * @param {array} date
+ * @return {string}
+ */
+function dateToString(date) {
+	if (date[0].length == 0) return "";
+	else date = date[0];
+	// The magic happens here
+	let year = date[0],
+		month = date[1],
+		day = date[2];
+	let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	return `${day} ${months[Number(month) - 1]}. ${year}`;
+}
+
+/*
+ * Handle the click event on the button
+ * @param {event} event
+ */
+function handleClick(event) {
+	// Get the URL from the input
+	const url = document.getElementById("url").value;
+	// Check if the URL is valid using regex
+	if (url.length == 0) return;
+	// Made the request and handle it
+	fetchData(url);
+}
+
+document.getElementById("btn").addEventListener("click", handleClick);
---
+++ readme.md
@@ -0,0 +1,17 @@
+## MLA7 Citation Generator
+
+### Project Inspiration
+
+I used too use [citationmachine](http://citationmachine.net/) to generate my MLA7. However it was really annoying to use and took a lot of time. So I simply wrote a detailed script to generate the same level of MLA7. 
+
+### Where can I view a live demo of my site
+
+You can view the demo at my [github page](https://www.fschatbot.github.io/MLA7)! I plan to add CSS and a simple config machine later on.
+
+### How I Generate my own Citation
+
+It's actually pretty simple. First I reverse engiered their API and simply wrote a small script to understand the JSON response. Then I wrote a script to generate the citation.
+
+### Credits
+
+Again because i use [citationmachine](http://citationmachine.net/) to generate my MLA7, I have no affliation with them.
