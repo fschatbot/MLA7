5eb4871228e2fe627c37665eff94291da2e80ad9
Fschatbot <s.himanshu.sultania@fountainheadschools.org>
2022-10-05 21:53:26 +0530

    Bumped TailwindCSS. Made a better dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["**/*.{html,js}"],
-	theme: { extend: {} },
+	content: ["*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				dark_text: "#DBEDF3",
+				dark_border: "#404B69",
+				dark_bg: "#283149",
+				dark_btn: "#00818A",
+				dark_btn_hover: "#16A094",
+			},
+		},
+	},
 	plugins: [],
 };
